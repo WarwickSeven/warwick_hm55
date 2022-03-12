8ff9b6a26cd3355062e3ccf91fac7441b1aac79d
WarwickSeven <WarwickSeven@gmail.com>
2022-03-12 23:08:15 +0300

    1

--- warwick_hm55/main.cpp
+++ warwick_hm55/main.cpp
@@ -1,14 +1,49 @@
-//
-//  main.cpp
-//  warwick_hm55
-//
-//  Created by Александр Тихонов on 12.03.2022.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool isCheckBalance1 (int *arr, int size, int *b)
+{
+    int x = 0, y = 0;
+    for (int i = 0; i < size-1; i++)
+    {
+        for (size_t j = 0; j <= i; j++)
+            x += arr[j];
+        for (size_t k = size-1; k > i; k--)
+            y += arr[k];
+        if (x==y)
+        {
+            *b = i+1;
+            break;
+        }
+        else
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+    return ((x == y)&&(x!=0));
+}
+
+
+void printArray (int *arr, int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+cout << endl;
+}
+
+int main()
+{
+    const int mySize = 8;
+    int myArr[mySize] = { 7,1,1,1,1,1,1,7 };
+    int b = 0;
+    printArray(myArr, mySize);
+    if (isCheckBalance1(myArr, mySize, &b))
+        cout << "баланс массива между " << b << " и " << b+1 << " элементами" << endl;
+    else
+        cout << "баланс отсутствует" << endl;
     return 0;
 }
